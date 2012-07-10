c7f84110bd6203df734468a310a9d6585310a34c
qusong <qusongdlzs@163.com>
2012-07-10 14:44:26 +0800

    0.2

---
+++ class/BannerStructBrowser.php
@@ -0,0 +1,34 @@
+<?php
+namespace org\opencomb\bannermt ;
+
+use org\opencomb\coresystem\auth\Id;
+use org\jecat\framework\mvc\controller\Controller;
+use org\opencomb\coresystem\mvc\controller\ControlPanel;
+
+class BannerStructBrowser extends ControlPanel
+{
+	
+	protected $arrConfig = array(
+		'title'=>'浏览模板编制项',
+		'view'=>array(
+			'template' => 'BannerStructBrowser.html' ,
+		) ,
+		'perms' => array(
+			// 权限类型的许可
+			'perm.purview'=>array(
+				'namespace'=>'coresystem',
+				'name' => Id::PLATFORM_ADMIN,
+			) ,
+		) ,
+	) ;
+
+	public function process()
+	{
+		$this->checkPermissions('您没有使用这个功能的权限,无法继续浏览',array()) ;
+		$this->view->variables()->set('bannername',$this->params->get('bannername'));
+	}
+    protected function defaultFrameConfig()
+    {
+    	return Controller::defaultFrameConfig() ;
+    }
+}
